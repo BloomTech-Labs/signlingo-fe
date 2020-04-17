7cc1efa5babb80fc807cf2dbf2cf208ecc18bf3d
Joey Nevarez <josephnevarez12@gmail.com>
2020-04-17 14:21:31 -0700

    adds signup form without the handleSubmit logic
    Co-authored-by: Erik Rodriguez <Eric19.21.03@gmail.com>
    Co-authored-by: Cedric Winbush <cawinbushjr@gmail.com>

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,67 @@
 import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as yup from "yup";
 
-const Signup = (props) => {
+let SignupSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email()
+    .required("This field is required."),
+  password: yup
+    .string()
+    .min(6, "Password is too short.")
+    .max(20, "Password is too long.")
+    .required("This field is required.")
+});
+
+const Signup = props => {
+  //the submit handler in formik, takes two parameters: the values (banana term), and formik bag
+  function submitHandler(values, { resetForm }) {
+    console.log("submitted", values);
+    //action creator
+    //reset the form?
+    values = {
+      email: "",
+      password: ""
+    };
+    console.log("reset values", values);
+  }
+
   return (
     <div>
-      <p>Sign Up</p>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          email: "",
+          password: ""
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={submitHandler}
+      >
+        {({ errors, handleChange, touched }) => (
+          <Form>
+            <Field
+              name="email"
+              type="email"
+              onChange={handleChange}
+              placeholder="email"
+            />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+
+            <Field
+              name="password"
+              type="password"
+              onChange={handleChange}
+              placeholder="password"
+            />
+            {errors.password && touched.password ? (
+              <div>{errors.password}</div>
+            ) : null}
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
