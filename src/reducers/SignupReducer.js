723fed1bfe978c24b374a7e6def17e0de8e88df4
Joey Nevarez <josephnevarez12@gmail.com>
2020-04-20 12:56:00 -0700

    adds signupreducer logic
    Co-authored-by: Erik Rodriguez <Eric19.21.03@gmail.com>

--- src/reducers/SignupReducer.js
+++ src/reducers/SignupReducer.js
@@ -1,7 +1,37 @@
-export const initialState = {};
+import {
+  SIGNUP_START,
+  SIGNUP_SUCCESS,
+  SIGNUP_ERROR
+} from "../actions/Signup";
+
+export const initialState = {
+  email: '',
+  password: '',
+  isLoading: false,
+  error: null
+};
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case SIGNUP_START:
+      return {
+      ...state,
+      isLoading: true, 
+      error: null
+      };
+    case SIGNUP_SUCCESS:
+      return {
+      ...state,
+      email: action.payload.email,
+      password: action.payload.password,
+      isLoading: false
+      };
+    case SIGNUP_ERROR:
+      return {
+      ...state,
+      isLoading: false,
+      error: action.payload
+      };
     default:
       return state;
   }
