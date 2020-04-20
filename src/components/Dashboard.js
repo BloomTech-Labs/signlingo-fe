0bbd592958979f0c4cb212d061e3e5dd7e25179b
Joey Nevarez <josephnevarez12@gmail.com>
2020-04-20 14:34:25 -0700

    adds logout button to dashboard co-author: <eric19.21.03@gmail.com>

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Redirect } from "react-router-dom";
 
+const Dashboard = props => {
+  function logout() {
+    window.localStorage.removeItem("token");
+    //redirect to whatever becomes the landing page
+    return <Redirect to="/" />;
+  }
 
-const Dashboard = (props) => {
   return (
     <div>
       <p>Dashboard Up</p>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 };
