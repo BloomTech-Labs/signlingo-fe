f8226c4b4878dce99064ba02d8c8fe87a47ae791
Joey Nevarez <josephnevarez12@gmail.com>
2020-04-20 14:07:15 -0700

    adds logic to login reducer co-author: <eric19.21.03@gmail.com>

--- src/reducers/LoginReducer.js
+++ src/reducers/LoginReducer.js
@@ -1,7 +1,33 @@
-export const initialState = {};
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_ERROR } from "../actions/Login";
 
-export const reducer = (state = initialState, action) => {
+export const initialState = {
+  email: "",
+  token: "",
+  isLoading: false,
+  error: null
+};
+
+export const loginReducer = (state = initialState, action) => {
   switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: null
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        email: action.payload.email,
+        token: action.payload.token,
+        isLoading: false
+      };
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
     default:
       return state;
   }
