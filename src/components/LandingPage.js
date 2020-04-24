8d729606d1b0d186538fb9a6a21d2c6ef93fef55
Erik Rodriguez <eric19.21.03@gmail.com>
2020-04-24 13:52:14 -0700

    Fixes to icon position with a padding top and font styles on slogan and logo on Landing Page component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -15,44 +15,79 @@ const LandingPage = () => {
     <>
       <div className="heroDiv">
         <img src={heroSm} alt="hand" />
-        <div id="logo"> <b>Sign</b><span>Lingo</span></div>
-        <div id="slogan">Learn to sign.<br/>One day at a time.</div>
-        <p id="login" onClick={() => history.push({pathname:"/account", state: {value: 1}})}>Log in</p>
+        <div id="logo">
+          {" "}
+          <b>Sign</b>
+          <span>Lingo</span>
+        </div>
+        <div id="slogan">
+          Learn to sign.
+          <br />
+          One day at a time.
+        </div>
+        <p
+          id="login"
+          onClick={() =>
+            history.push({ pathname: "/account", state: { value: 1 } })
+          }
+        >
+          Log in
+        </p>
       </div>
-  
+
       <div className="landingContent">
-        <div id="landingBtn" onClick={() => history.push({pathname:"/account", state: {value: 0}})}>
+        <div
+          id="landingBtn"
+          onClick={() =>
+            history.push({ pathname: "/account", state: { value: 0 } })
+          }
+        >
           Sign up
         </div>
 
         <div className="landingBoxes">
           <section>
-            <div><img src={medalSm} alt="medal icon" /></div>
+            <div>
+              <img src={medalSm} alt="medal icon" />
+            </div>
             <b>Fun gamified lessons</b>
-            <p>Get a hot streak going, track your progress, earn points and lose hearts.</p>
+            <p>
+              Get a hot streak going, track your progress, earn points and lose
+              hearts.
+            </p>
           </section>
 
           <section>
-            <div><img src={personSm} alt="person icon" /></div>
+            <div>
+              <img src={personSm} alt="person icon" />
+            </div>
             <b>Easy online learning</b>
-            <p>SignLingo is available on mobile and desktop for your convenience!</p>
+            <p>
+              SignLingo is available on mobile and desktop for your convenience!
+            </p>
           </section>
 
           <section>
-            <div><img src={examSm} alt="exam icon" /></div>
+            <div>
+              <img src={examSm} alt="exam icon" />
+            </div>
             <b>End of unit quizzes </b>
             <p>Quizzes at the end of each unit will help track your growth.</p>
-            </section>
+          </section>
 
           <section>
-            <div><img src={recSm} alt="rec icon"/></div>
+            <div>
+              <img src={recSm} alt="rec icon" />
+            </div>
             <b>Video recognition</b>
-            <p>Our recognition software will tell you if you are signing correctly.</p>
+            <p>
+              Our recognition software will tell you if you are signing
+              correctly.
+            </p>
           </section>
         </div>
       </div>
     </>
-
   );
 };
 
