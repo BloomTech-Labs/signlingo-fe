5aace8c10e52343f3b42dc7d06a802b892437b43
cedric-winbush <cawinbushjr@gmail.com>
2020-04-29 11:39:40 -0500

    adds Material UI progress bar to dashboardcard.js

--- src/components/DashboardCard.js
+++ src/components/DashboardCard.js
@@ -1,12 +1,42 @@
 import React from "react";
 // import { Redirect } from 'react-router-dom';
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { lighten, makeStyles, withStyles } from "@material-ui/core/styles";
+
 import crownSm from "../images/icons/progress crown icon.png";
 import practiceSm from "../images/icons/practiceSm.png";
 import lessonSm from "../images/icons/lessonSm.png";
 import quizSm from "../images/icons/quizSm.png";
 import progressSm from "../images/icons/progress bar icon.png";
 
+const BorderLinearProgress = withStyles({
+  root: {
+    height:15,
+    width: '100%',
+    backgroundColor: lighten("#ff6c5c", 0.5),
+    maxWidth: '100%'
+   // margin: 0,
+  },
+  bar: {
+    borderRadius: 20,
+    backgroundColor: "#ff6c5c",
+   // margin: 0,
+
+  },
+})(LinearProgress);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  margin: {
+   // margin: theme.spacing(1),
+    margin: 'auto'
+  },
+}));
+
 const DashboardCard = (props) => {
+  const classes = useStyles();
   function lessonHandler() {
     return;
   }
@@ -24,6 +54,18 @@ const DashboardCard = (props) => {
       <h1>Alphabet - Level {props.data.level.number}</h1>
       <div className="progressBar">
         <img src={progressSm} alt="progress bar" />
+        <img src={crownSm} alt="A completion crown" />
+      </div>
+      <div className="progressBar">
+        <div style={{ width: '90%'}}>
+          <BorderLinearProgress
+            className={classes.margin}
+            variant="determinate"
+            color="secondary"
+            value={0}
+          />
+        </div>
+        
         <img src={crownSm} alt="A completion crown" />
       </div>
 
