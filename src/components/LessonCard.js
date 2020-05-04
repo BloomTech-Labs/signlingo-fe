723249c5c47a7a25d0122e379f73adf5846b6740
Erik Rodriguez <eric19.21.03@gmail.com>
2020-05-04 13:46:03 -0700

    Adds card flip animation to lesson card component Co-authored-by: Joey Nevarez Cedric Winbush <josephnevarez12@gmail.com> <cawinbushjr@gmail.com>

--- src/components/LessonCard.js
+++ src/components/LessonCard.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import test from "../images/icons/SignA.png";
 
 const LessonCard = (props) => {
   const [imageOn, setImageOn] = useState(false);
 
+  function flipCard() {
+    setImageOn(!imageOn);
+  }
+
   return (
-    <div className="flash" onClick={() => setImageOn(!imageOn)}>
-      {imageOn ? (
-        <p className="flashImage">{props.data.img}</p>
-      ) : (
-        <p className="flashLetter">{props.data.letter}</p>
-      )}
+    <div className="lessonCards">
+      <ReactCardFlip isFlipped={imageOn}>
+        <div onClick={flipCard} className="frame">
+          <img src={test} alt="test" />
+        </div>
+        <div onClick={flipCard} className="frame">
+          {props.data.letter}
+        </div>
+      </ReactCardFlip>
     </div>
   );
 };
