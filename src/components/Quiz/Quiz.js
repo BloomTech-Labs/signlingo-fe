e2effa4fce2e8e7fa015353ae03403ca6d3c9770
cjogles <jackson.ogles28@gmail.com>
2020-05-26 11:58:41 -0600

    Layed out logic for next button and updated state for quiz component
    Co-authored-by: Natalia Beckstead <toosmiravilla@gmail.com>
    Co-authored-by: biskoi <biskoi@daydream.cafe>
    Co-authored-by: Bryan Bilek <bbilek35@gmail.com>
    Co-authored-by: Michael Famurewa <myfamurewa@gmail.com>

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -6,7 +6,29 @@ import VideoAssessment from "./VideoAssessment";
 //  Need to build up overlays
 //  Idea for currentTestValue: either passed down as props, or read from the URL params
 const Quiz = (props) => {
+
+  const data = ['a', 'b', 'c', 'd', 'e'];
   const [videoOn, setVideoOn] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  let score = 0;
+  let enableButton = false;
+  // state ideas
+  // nextButtonTrue/False, 
+  // incrementing integer to keep track of user score, 
+  // passing down a function to VideoAssessment to manipulate integer
+  // videoAssessment needs to be able to turn the next button on after getting results back from DS API
+  // Next button needs to be turned on after each letter
+
+  let scoreHandler = () => {
+    return score++;
+  };
+
+  let nextHandler = () => {
+    if (enableButton) {
+      setCurrentIndex(currentIndex + 1)
+      enableButton = false;
+    }
+  }
 
   const turnVideoOn = () => {
     setVideoOn(true);
@@ -23,17 +45,18 @@ const Quiz = (props) => {
         />
         <img className="heart" src="./images/heart.png" alt="heart image" />
       </div>
-      <h1 className="signLabel">Sign "A"</h1>
+  <h1 className="signLabel">Sign {data[currentIndex]}}</h1>
       {videoOn ? (
-        <VideoAssessment testValue="currentTestValue" />
+        <VideoAssessment testValue = {data[currentIndex]} scoreHandler = {scoreHandler} />
       ) : (
         <div className="cameraOverlay">
           <img onClick={turnVideoOn} src="./images/openCamOverlay.png" alt="turns camera on"></img>
         </div>
       )}
-      <button className="nextButton">Next</button>
+      <button onClick={nextHandler} disabled={!enableButton} className={enableButton ? "nextButton" : "disableNextButton"}>Next</button> 
     </div>
   );
 };
 
 export default Quiz;
+
