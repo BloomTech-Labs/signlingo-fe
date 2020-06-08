2d9a3fc050a200a8f25e6139d13abea5ecc4baf6
cjogles <jackson.ogles28@gmail.com>
2020-06-08 17:40:35 -0600

    finished logic for dashboard images

--- src/components/Dashboard/DashboardCard.js
+++ src/components/Dashboard/DashboardCard.js
@@ -6,60 +6,61 @@ const DashboardCard = (props) => {
   const history = useHistory();
   const purl = process.env.PUBLIC_URL;
   const alphabet = 5;
+  const d = props.levelData;
+  let flashcard;
+  let exercise;
+  let quiz;
+  function findWithAttr(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+  let levelMinusOne = findWithAttr(props.userLevels, "level_id", d.level_id - 1)
+  if (d.level_id === 1) {
+    if (d.completed_flashcards === null) {
+      flashcard = <img src={purl + "/images/icons/flashCardColor.png"}></img>
+    } else if (d.completed_flashcards) {
+      flashcard = <img src={purl + "/images/icons/flashCardChecked.png"}></img>
+    }
+  }
+  else {
+    if (props.userLevels[levelMinusOne].completed_exercises && props.userLevels[levelMinusOne].completed_flashcards && props.userLevels[levelMinusOne].completed_quiz) {
+      flashcard = <img src={purl + "/images/icons/flashCardColor.png"}></img>
+    } else if (d.completed_flashcards) {
+      flashcard = <img src={purl + "/images/icons/flashCardChecked.png"}></img>
+    }
+    else {
+      flashcard = <img src={purl + "/images/icons/flashCardFaded.png"}></img>
+    }
+  }
+
+  if (d.completed_flashcards !== null && d.completed_exercises === null){
+    exercise = <img src={purl + "/images/icons/exerciseColor.png"}></img>
+  } else if (d.completed_flashcards && d.completed_exercises) {
+    exercise = <img src={purl + "/images/icons/exerciseChecked.png"}></img>
+  } else {
+     exercise = <img src={purl + "/images/icons/exerciseFaded.png"}></img>
+  }
+
+  if (d.completed_quiz === null && d.completed_exercises){
+    quiz = <img src={purl + "/images/icons/quizColor.png"}></img>
+  } else if (d.completed_quiz && d.completed_exercises) {
+    quiz = <img src={purl + "/images/icons/quizChecked.png"}></img>
+  } else {
+    quiz = <img src={purl + "/images/icons/quizFaded.png"}></img>
+  }
+
+
 
   return (
     <>
       {props.title <= alphabet ? `Alphabet - Level ${props.title}` : null}
-      {props.levelData.completed_flashcards === null &&
-      props.levelData.completed_exercises === null &&
-      props.levelData.completed_quiz === null ? (
-        <>
-          <img src={purl + "/images/icons/flashCardFaded.png"}></img>
-          <img src={purl + "/images/icons/exerciseFaded.png"}></img>
-          <img src={purl + "/images/icons/quizFaded.png"}></img>
-        </>
-      ) : (
-        <>
-          <p>display active levels accordingly</p>
-          // for flashcard
-          {props.levelData.completed_flashcards !== null ? (
-            <img src={purl + "/images/icons/flashCardChecked.png"}></img>
-          ) : (
-            <>
-              <img src={purl + "/images/icons/flashCardFaded.png"}></img>
-              <img src={purl + "/images/icons/exerciseFaded.png"}></img>
-              <img src={purl + "/images/icons/quizFaded.png"}></img>
-            </>
-          )}
-          // for excercise
-          {props.levelData.completed_exercises !== null ? (
-            <img src={purl + "/images/icons/exerciseChecked.png"}></img>
-          ) : (
-            <p>exercise is null</p>
-          )}
-          // for quiz
-          {props.levelData.completed_quiz !== null ? (
-            <img src={purl + "/images/icons/flashCardChecked.png"}></img>
-          ) : (
-            <p>quiz is null</p>
-          )}
-        </>
-      )}
-
-      {/* <img src={purl + "/images/icons/progressBarColor.png"}></img>
-    <img src={purl + "/images/icons/crownColor.png"}></img>
-
-    <img src={purl + "/images/icons/flashCardColor.png"}></img>
-    <img src={purl + "/images/icons/exerciseColor.png"}></img>
-    <img src={purl + "/images/icons/quizColor.png"}></img>
-
-    <img src={purl + "/images/icons/flashCardFaded.png"}></img>
-    <img src={purl + "/images/icons/exerciseFaded.png"}></img>
-    <img src={purl + "/images/icons/quizFaded.png"}></img>
-
-    <img src={purl + "/images/icons/flashCardChecked.png"}></img>
-    <img src={purl + "/images/icons/exerciseChecked.png"}></img>
-    <img src={purl + "/images/icons/quizChecked.png"}></img> */}
+      {flashcard}
+      {exercise}
+      {quiz}
     </>
   );
 };
