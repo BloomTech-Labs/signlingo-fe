a18442e1bdb5399bd85b5c532961ec4d0732335a
cjogles <jackson.ogles28@gmail.com>
2020-06-08 21:13:11 -0600

    styling for dashboard added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -10,7 +10,7 @@ import DashboardCard from "./DashboardCard";
 
 const Dashboard = (props) => {
   const history = useHistory();
-  console.log("outside", props.levels.length, props.userLevels.length)
+  console.log("outside", props.levels.length, props.userLevels.length);
   function logout() {
     localStorage.removeItem("userID", "token");
     history.push("/");
@@ -18,26 +18,32 @@ const Dashboard = (props) => {
   useEffect(() => {
     props.getAllUserLevelsByID();
     props.getAllLevels();
-    console.log("inside", props.levels.length, props.userLevels.length)
+    console.log("inside", props.levels.length, props.userLevels.length);
     if (props.levels.length > props.userLevels.length) {
-      console.log("conditional", props.levels.length, props.userLevels.length)
+      console.log("conditional", props.levels.length, props.userLevels.length);
       props.addLevelsToUserAccount(props.levels, props.userLevels);
     } else if (props.levels.length < props.userLevels.length) {
       throw new Error("This should be unreachable");
     }
-  
   }, [props.levels.length]);
 
   return (
-    <div>
-      <span onClick={logout}>Log Out</span>
+    <div className="dashboard-wrapper">
+      <span className="logout-span">
+        <p className="logout-filler"></p>
+        <p className="logout"onClick={logout}>Log Out</p>
+      </span>
       {props.isLoading ? (
         <>
           <p>{props.loadingMessage}</p>
         </>
       ) : (
         props.userLevels.map((levelData) => (
-          <DashboardCard key={levelData.id} levelData={levelData} title={levelData.level_id}/>
+          <DashboardCard
+            key={levelData.id}
+            levelData={levelData}
+            title={levelData.level_id}
+          />
         ))
       )}
     </div>
--- src/components/Dashboard/DashboardCard.js
+++ src/components/Dashboard/DashboardCard.js
@@ -10,58 +10,136 @@ const DashboardCard = (props) => {
   let flashcard;
   let exercise;
   let quiz;
+
   function findWithAttr(array, attr, value) {
-    for(var i = 0; i < array.length; i += 1) {
-        if(array[i][attr] === value) {
-            return i;
-        }
+    for (var i = 0; i < array.length; i += 1) {
+      if (array[i][attr] === value) {
+        return i;
+      }
     }
     return -1;
-}
-  let levelMinusOne = findWithAttr(props.userLevels, "level_id", d.level_id - 1)
+  }
+
+  let levelMinusOne = findWithAttr(
+    props.userLevels,
+    "level_id",
+    d.level_id - 1
+  );
+
   if (d.level_id === 1) {
     if (d.completed_flashcards === null) {
-      flashcard = <img src={purl + "/images/icons/flashCardColor.png"}></img>
+      flashcard = (
+        <>
+          <img src={purl + "/images/icons/flashCardColor.png"}></img>
+          <p>Flashcards</p>
+        </>
+      );
     } else if (d.completed_flashcards) {
-      flashcard = <img src={purl + "/images/icons/flashCardChecked.png"}></img>
+      flashcard = (
+        <>
+          <img src={purl + "/images/icons/flashCardChecked.png"}></img>
+          <p>Flashcards</p>
+        </>
+      );
     }
-  }
-  else {
-    if (props.userLevels[levelMinusOne].completed_exercises && props.userLevels[levelMinusOne].completed_flashcards && props.userLevels[levelMinusOne].completed_quiz) {
-      flashcard = <img src={purl + "/images/icons/flashCardColor.png"}></img>
+  } else {
+    if (
+      props.userLevels[levelMinusOne].completed_exercises &&
+      props.userLevels[levelMinusOne].completed_flashcards &&
+      props.userLevels[levelMinusOne].completed_quiz
+    ) {
+      flashcard = (
+        <>
+          <img src={purl + "/images/icons/flashCardColor.png"}></img>
+          <p>Flashcard</p>
+        </>
+      );
     } else if (d.completed_flashcards) {
-      flashcard = <img src={purl + "/images/icons/flashCardChecked.png"}></img>
-    }
-    else {
-      flashcard = <img src={purl + "/images/icons/flashCardFaded.png"}></img>
+      flashcard = (
+        <>
+          <img src={purl + "/images/icons/flashCardChecked.png"}></img>
+          <p>Flashcard</p>
+        </>
+      );
+    } else {
+      flashcard = (
+        <>
+          <img src={purl + "/images/icons/flashCardFaded.png"}></img>
+          <p>Flashcard</p>
+        </>
+      );
     }
   }
 
-  if (d.completed_flashcards !== null && d.completed_exercises === null){
-    exercise = <img src={purl + "/images/icons/exerciseColor.png"}></img>
+  if (d.completed_flashcards !== null && d.completed_exercises === null) {
+    exercise = (
+      <>
+        <img src={purl + "/images/icons/exerciseColor.png"}></img>
+        <p>Exercises</p>
+      </>
+    );
   } else if (d.completed_flashcards && d.completed_exercises) {
-    exercise = <img src={purl + "/images/icons/exerciseChecked.png"}></img>
+    exercise = (
+      <>
+        <img src={purl + "/images/icons/exerciseChecked.png"}></img>
+        <p>Exercises</p>
+      </>
+    );
   } else {
-     exercise = <img src={purl + "/images/icons/exerciseFaded.png"}></img>
+    exercise = (
+      <>
+        <img src={purl + "/images/icons/exerciseFaded.png"}></img>
+        <p>Exercises</p>
+      </>
+    );
   }
 
-  if (d.completed_quiz === null && d.completed_exercises){
-    quiz = <img src={purl + "/images/icons/quizColor.png"}></img>
+  if (d.completed_quiz === null && d.completed_exercises) {
+    quiz = (
+      <>
+        <img src={purl + "/images/icons/quizColor.png"}></img>
+        <p>Video Quiz</p>
+      </>
+    );
   } else if (d.completed_quiz && d.completed_exercises) {
-    quiz = <img src={purl + "/images/icons/quizChecked.png"}></img>
+    quiz = (
+      <>
+        <img src={purl + "/images/icons/quizChecked.png"}></img>
+        <p>Video Quiz</p>
+      </>
+    );
   } else {
-    quiz = <img src={purl + "/images/icons/quizFaded.png"}></img>
+    quiz = (
+      <>
+        <img src={purl + "/images/icons/quizFaded.png"}></img>
+        <p>Video Quiz</p>
+      </>
+    );
   }
-
-
 
   return (
-    <>
-      {props.title <= alphabet ? `Alphabet - Level ${props.title}` : null}
-      {flashcard}
-      {exercise}
-      {quiz}
-    </>
+    <div className="dashboard">
+      {props.title <= alphabet ? <h1>Alphabet - Level {props.title}</h1> : null}
+      <div className="progress-bar">
+        <img
+          className="progress-bar-img"
+          src={purl + "/images/icons/progressBarColor.png"}
+          alt="A progress Bar"
+        />
+        <img
+          className="crown-img"
+          src={purl + "/images/icons/crownColor.png"}
+          alt="A completion crown"
+        />
+      </div>
+      <section className="dashboard-content">
+        <div id="flashcard-box">{flashcard}</div>
+        <div className="exercise-quiz-wrapper">
+          <div id="exercise-box">{exercise}</div>
+          <div id="quiz-box">{quiz}</div>
+        </div>
+      </section>
+    </div>
   );
 };
 
