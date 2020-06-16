ab3afcdf9b05fea0a39a4ea34fd831ac2a9c8dee
cjogles <jackson.ogles28@gmail.com>
2020-06-16 11:05:42 -0600

    styling for exercise wrapper x image and progressbar image and heart image

--- src/components/Exercises/ExerciseWrapper.js
+++ src/components/Exercises/ExerciseWrapper.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
@@ -6,30 +6,56 @@ import Exercise from "./ExerciseCard";
 const URL = process.env.REACT_APP_BACK_END_BASE_URL;
 
 const ExerciseWrapper = (props) => {
-    const { id } = useParams();
-    let history = useHistory();
-    useEffect(() => {
-        axios
-          .get(`${URL}flashcards/${id}`)
-          .then((res) => {
-            setFlashcardData(res.data);
-          })
-          .catch((err) => {});
-      }, []);
+  const { id } = useParams();
+  let history = useHistory();
+  const [flashcardData, setFlashcardData] = useState([]);
 
-    return (
-        <>
-            
-        </>
-    );
+  function backToDash() {
+    return history.push("/dashboard");
+  }
+
+  useEffect(() => {
+    axios
+      .get(`${URL}flashcards/${id}`)
+      .then((res) => {
+        setFlashcardData(res.data);
+      })
+      .catch((err) => {});
+  }, []);
+
+  return (
+    <>
+      <div className="exercise">
+        <div className="exerciseBar">
+          <img
+            src={process.env.PUBLIC_URL + "/images/icons/x.png"}
+            alt="letter x"
+            onClick={backToDash}
+          />
+        </div>
+        <div className="livesBar">
+          <img
+            className="progressBarExercise"
+            src={process.env.PUBLIC_URL + "/images/icons/progressBarColor.png"}
+            alt="A progress Bar"
+          />
+          <img
+            className="heartExercise"
+            src={process.env.PUBLIC_URL + "/images/exercises/heart.png"}
+            alt="A heart and lives count"
+          />
+        </div>
+        {flashcardData &&
+          flashcardData.map((each) => <Exercise key={each.id} data={each} />)}
+      </div>
+    </>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        
-    };
-  };
-  
-  const mapDispatchToProps = {};
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(ExerciseWrapper);
+  return {};
+};
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExerciseWrapper);
